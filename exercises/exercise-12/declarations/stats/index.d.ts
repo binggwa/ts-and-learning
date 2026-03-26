f512a34c958ad667eb38a8e1bf7d947b8799c1f4
binggwa <qudrhks317@gmail.com>
2026-03-26 15:51:30 +0900

    chore(exercise-12) : 타입명 카멜케이스>파스칼케이스로 변경

--- exercises/exercise-12/declarations/stats/index.d.ts
+++ exercises/exercise-12/declarations/stats/index.d.ts
@@ -1,16 +1,16 @@
 declare module 'stats' {
     type Comparator<T> = (a: T, b: T) => number;
     
-    type getIndex = <T>(input: T[], comparator: Comparator<T>) => number;
+    type GetIndex = <T>(input: T[], comparator: Comparator<T>) => number;
 
-    export const getMaxIndex: getIndex;
-    export const getMinIndex: getIndex;
-    export const getMedianIndex: getIndex;
+    export const getMaxIndex: GetIndex;
+    export const getMinIndex: GetIndex;
+    export const getMedianIndex: GetIndex;
 
-    type getElement = <T>(input: T[], comparator: Comparator<T>) => null | T;
-    export const getMaxElement: getElement;
-    export const getMinElement: getElement;
-    export const getMedianElement: getElement;
+    type GetElement = <T>(input: T[], comparator: Comparator<T>) => null | T;
+    export const getMaxElement: GetElement;
+    export const getMinElement: GetElement;
+    export const getMedianElement: GetElement;
     
     export const getAverageValue: <T>(input: T[], getValue: (item: T) => number) => null | number;
 }
